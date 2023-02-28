b05a650d9cd46c9af53694a3591a9fb4be310f47
mattsurtees996 <matthew.surtees@icloud.com>
2023-02-28 04:45:09 +0000

    finish

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,11 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
 export default function HornedBeast({ title, imageURL, description }) {
+  const [votes, setVotes] = useState(0);
+
+  function handleVote() {
+    setVotes(votes + 1);
+  }
+
   return (
     <div className="horned-beasts">
       <h2>{title}</h2>
-      <img src={imageURL} alt="" />
+      <img src={imageURL} alt="" onClick={handleVote} />
       <p>{description}</p>
+      <p>❤️ {votes} </p>
     </div>
   );
 }
